0bc6c78217d809758bc6efdd2953f015f063549e
engrlindagift <lindagift0812@gmail.com>
2023-05-10 07:59:10 +0000

    Task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Custom User Agent"}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()['data']['subscribers']
         return data
