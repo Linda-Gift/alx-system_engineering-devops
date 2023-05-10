4c7b4885b8e9a33757a90772d3b53eb9a2b40851
engrlindagift <lindagift0812@gmail.com>
2023-05-10 06:21:01 +0000

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A function that queries Reddit API and returns top ten hot post"""
+
+import requests
+import sys
+
+def top_ten(subreddit):
+    """This func. takes one parameter"""
+    subreddit = argv[1];
+
+    url = ("https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit))
+    headers = {"User-Agent": "Custom User Agent"}
+
+    response = requests.get(url, headers = headers, allow_redirections=False)
+    if response.status_code == 200:
+        data = response.json()['data']['children']
+        for post in data:
+            print(post['data']['title'])
+        else:
+            print(None)
