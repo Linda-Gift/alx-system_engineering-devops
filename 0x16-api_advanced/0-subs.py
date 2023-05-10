96df87d9acaa71d0565f6d216bd4f2a6ad89376c
engrlindagift <lindagift0812@gmail.com>
2023-05-10 06:10:29 +0000

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A function that queries Reddit API and returns num. of subs"""
+
+import requests
+import sys
+
+def number_of_subscribers(subreddit):
+    """This func. takes one parameter"""
+    subreddit = sys.argv[1];
+
+    url = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
+    headers = {"User-Agent": "Custom User Agent"}
+
+    response = requests.get(url, headers = headers, allow_redirections=False)
+    if response.status_code == 200:
+        data = response.json()['data']['subscribers']
