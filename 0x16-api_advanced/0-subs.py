18c6ab059093d3f2e1fe4adcfa9d12ce196cf2ae
engrlindagift <lindagift0812@gmail.com>
2023-05-10 08:41:24 +0000

    Task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,6 @@ import sys
 
 def number_of_subscribers(subreddit):
     """This func. takes one parameter"""
-    subreddit = sys.argv[1]
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Custom"}
