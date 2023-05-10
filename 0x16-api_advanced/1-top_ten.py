f1ab78b5e9e092af255fb7a34009252cb2e87a2c
engrlindagift <lindagift0812@gmail.com>
2023-05-10 08:16:08 +0000

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Custom User Agent"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()['data']['children']
         for post in data:
