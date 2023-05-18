ec18c624af9edf23f46cdbf6d98a8bb4f4bca7b7
engrlindagift <lindagift0812@gmail.com>
2023-05-18 08:51:10 +0000

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-#This conf. is to review the number of requests made to nginx
+#puppet manifest to modify the num of requests made to nginx
 file { '/etc/default/nginx':
   ensure  => file,
   content => "ULIMIT='-n 3072'\n",
