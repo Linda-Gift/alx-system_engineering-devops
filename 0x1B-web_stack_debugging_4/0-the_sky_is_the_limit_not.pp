dd46749f4dfc2af7ec3beefdc9733c30f66a948c
Linda Gift <110992464+engrlindagift@users.noreply.github.com>
2023-05-19 05:20:49 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,5 @@
-# puppet manifest to change no of req.
-$filename = '/etc/default/nginx'
-$ulimit_value = '3072'
-
-file { $filename:
+# puppet manifest to change no of req
+file { '/etc/default/nginx':
   ensure  => file,
-  content => "ULIMIT='-n ${ulimit_value}'\n",
-}
-
-# URL encode the file path for linting
-$encoded_filename = URI.encode_www_form_component($filename)
-exec { 'puppet_lint':
-  command => "puppet lint ${encoded_filename}",
-  path    => '/usr/bin',
-  unless  => "puppet-lint ${encoded_filename}",
+  content => "ULIMIT='-n 3072'\n",
 }
